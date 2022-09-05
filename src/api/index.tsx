4a6b4db5d8186cb9ec9bfd5915e7b511169539c1
dmitry <ds@gmail.com>
2022-09-05 20:02:14 +0300

    refactor: api route

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const EmployeesAPI = {
   fetchAllEmployees: async () => {
     try {
-      const response = await axios(`api/tasks/users`);
+      const response = await axios(`/api/tasks/users`);
       return response;
     } catch (e) {
       throw e;
