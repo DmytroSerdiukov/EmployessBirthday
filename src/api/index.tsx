572c8402539b18cabe9a37e0076c5b878e041216
dmitry <ds@gmail.com>
2022-09-05 22:06:31 +0300

    feature: redirects

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const EmployeesAPI = {
   fetchAllEmployees: async () => {
     try {
-      const response = await axios(`http://topdevsprojects.org/tasks/users`);
+      const response = await axios(`/tasks/users`);
       return response;
     } catch (e) {
       throw e;
